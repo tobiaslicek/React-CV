55c82d8f2315bea145d6c61279524a8ac1250df9
Tobiáš Licek <licek@voatt.cz>
2025-10-18 09:58:47 +0200

    fix: refactor Header component for improved readability and consistency

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React from "react";
 
-const Header = ({ name, contacts }) => {
-  return React.createElement(
-    'header',
-    {
-      className: 'cv-header',
-    },
-    [
-      React.createElement(
-        'h1',
+const Header = ({name, contacts}) => {
+    return React.createElement(
+        'header',
         {
-          className: 'cv-name',
+            className: 'cv-header'
         },
-        'Jan Novák'
-      ),
-    ]
-  );
-};
+        [
+            React.createElement(
+                'h1',
+                {
+                    className: 'cv-name'
+                },
+                "Jan Novák"
+            ),
+            React.createElement(
+                "p",
+                {
+                    key: "contact",
+                    className: "cv-contact",
+                },
+                "📧 jan.novak@example.com | ☎️ +420 777 888 999 | 🌐 www.jannovak.cz"
+            ),
+            React.createElement(
+                'img',
+                {
+                    className: 'cv-photo',
+                    src: 'https://flowbite.com/docs/images/people/profile-picture-5.jpg',
+                    alt: "Profilová fotka"
+                }
+            )
+        ],
+    )
+}
 
 export default Header;
