848f3bc2e6359d6e9e8fbd3893c551a55fb544de
Tobiáš Licek <licek@voatt.cz>
2025-10-30 18:33:00 +0100

    feat: add ErrorBoundary component for error handling in the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Router } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 
-import CVRoute from './App.jsx'
+import CVRoute from './App.jsx';
 
-import './index.css'
+import './index.css';
+import ErrorBoundary from './components/errorBoundary.jsx';
 
-const App = () => <Router>
-  <Route
-</Router>
+const App = () => (
+  <Router>
+    <Route path="/" element={<CVRoute />} />
+  </Router>
+);
 
 const root = document.getElementById('root');
 createRoot(root).render(
   <StrictMode>
-    <BrowserRouter>
+    <ErrorBoundary>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </ErrorBoundary>
+  </StrictMode>
 );
