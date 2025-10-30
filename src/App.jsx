56397e12874080dcbf40f77ec1f6e190bf7a6fd9
Tobiáš Licek <licek@voatt.cz>
2025-10-30 19:23:52 +0100

    feat: integrate jobs, skills, and links data from JSON into CVPage component

--- src/App.jsx
+++ src/App.jsx
@@ -11,86 +11,12 @@ import { useSearchParams, useNavigate } from 'react-router-dom';
 import data from './data/profile.json';
 
 /**
- * Seznam pracovních zkušeností
- */
-const jobs = [
-  {
-    icon: 'https://cdn-icons-png.flaticon.com/512/906/906343.png',
-    title: 'Frontend Developer – ABC s.r.o.',
-    period: '2023 / 01 – 2025 / 03',
-    description:
-      'Na této pozici jsem se podílel na vývoji webových aplikací a mobilních aplikací a kopal kanály a nasazoval jsem na ně víka.',
-    skills: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'React',
-      'Go',
-      'Node.js',
-      'PHP',
-      'Git',
-      'GitHub',
-      'Gitlab',
-      'VSCode',
-    ],
-  },
-  {
-    icon: 'https://cdn-icons-png.flaticon.com/512/906/906175.png',
-    title: 'Web Designer – Freelance',
-    period: '2021 / 05 – 2022 / 12',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    skills: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'React',
-      'Go',
-      'Node.js',
-      'PHP',
-      'Git',
-      'GitHub',
-      'Gitlab',
-      'VSCode',
-    ],
-  },
-];
-
-/**
- * Seznam dovedností
- */
-const skills = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'Typescript',
-  'React',
-  'Go',
-  'Node.js',
-  'PHP',
-  'Git',
-  'GitHub',
-  'Gitlab',
-  'VSCode',
-];
-
-/**
- * Odkazy v patičce
- */
-const links = [
-  { label: 'GitHub', url: 'https://github.com/sjiamnocna' },
-  { label: 'Gitlab', url: 'https://gitlab.com/sjiamnocna' },
-  { label: 'LinkedIn', url: 'https://linkedin.com/in/sjiamnocna' },
-  { label: 'Portfolio', url: 'https://jancsi.cz' },
-];
-
-/**
  * Hlavní komponenta aplikace
  */
 const CVPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const { personalInfo } = data;
+  const { personalInfo, jobs, skills, links } = data;
 
   // Get job index from URL parameter
   const jobIndex = searchParams.get('job');
