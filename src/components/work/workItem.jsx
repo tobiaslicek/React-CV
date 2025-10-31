0b9f39ae4e7ccf8bf431dae5b00026f14b109357
Tobiáš Licek <licek@voatt.cz>
2025-10-31 15:12:34 +0100

    Create work section

---
+++ src/components/work/workItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WorkItem = ({ icon, title, period, ...props }) => {
+  return (
+    <li className="work-item" {...props}>
+      <img src={icon} alt="ikona práce" className="work-item-icon" />
+      <div className="work-item-content">
+        <span className="work-item-title">{title}</span>
+        <span className="work-item-period">{period}</span>
+      </div>
+    </li>
+  );
+};
+
+export default WorkItem;
