719acd3bfedce7a021f6c84dacdf1dd6dc010e01
Tobiáš Licek <licek@voatt.cz>
2025-11-05 08:04:02 +0100

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import CVPage from './pages/cv/cv.page';
 import ProfilePage from './pages/profile/profile.page';
 
-import './index.css'
-import './App.css'
+import './index.css';
+import './App.css';
 import ProjectsPage from './pages/projects/projects.page';
+import ProjectDetailPage from './pages/projects/projectDetail.page';
 
 const App = () => {
-  return <Routes>
-    <Route path="/" element={<CVPage />} />
-    <Route path="/profile" element={<ProfilePage />} />
-    <Route path='/projects' element={<ProjectsPage />} />
-    <Route path='/project/:projectID' element={} />
-    <Route path="*" Component={() => <div>404 Not Found</div>} />
-  </Routes>
-}
+  return (
+    <Routes>
+      <Route path="/" element={<CVPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/project/:projectID" element={<ProjectDetailPage />} />
+      <Route path="*" Component={() => <div>404 Not Found</div>} />
+    </Routes>
+  );
+};
 
 export default App;
